de01faf47c70bcdfd6aeadadad70e52ee31c1257
Davis.Du <davis.du@bluecomgroup.com>
2016-05-26 11:11:23 +0800

    aa

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,24 @@ import com.nccc.evpos.HppApiClient;
 
 public class HelloWorld {
   public static void main(String args[]) throws Exception {
-         // 產生 API 物件
          HppApiClient apiClient=new HppApiClient();
-         return HppApiClient;
+         apiClient.setMERCHANTID("6600800020");
+         apiClient.setTERMINALID("70502876");
+         apiClient.setORDERID("1000001");
+         apiClient.setTRANSMODE("0");
+         apiClient.setINSTALLMENT("0");
+         apiClient.setTRANSAMT("1");
+         apiClient.setNotifyURL("");
+         apiClient.setURL("nccnet-ectest.nccc.com.tw", "/merchant/HPPRequest");
+         int rtnCode=apiClient.postTransaction();
+         System.out.println(rtnCode);
+         String responseCode=apiClient.getRESPONSECODE();
+         if ("00".equals(responseCode)){
+         try {
+            System.out.println(apiClient.getKEY());
+         } catch (Exception ex){}
+         } else {
+         System.out.println("Error");
+         }
     }
   }
